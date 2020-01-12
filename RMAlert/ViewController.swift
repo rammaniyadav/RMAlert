bb51eb1186b688b8fedf513a958ff82f4cceba83
Ram Mani Yadav <rammani.yadav@appinventiv.com>
2020-01-12 11:16:29 +0530

    Git ignore added

---
+++ RMAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RMAlert
+//
+//  Created by Ram Mani  on 12/01/20.
+//  Copyright © 2020 Appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
